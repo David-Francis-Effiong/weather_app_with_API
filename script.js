c244aba6ccb937c6402f7a3b781bedf73bea9861
David Francis Effiong <92728261+David-Francis-Effiong@users.noreply.github.com>
2025-06-30 10:45:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-var searchInput = document.getElementById ('searchInput')
-var submitButton = document.getElementById ('submit-btn')
-var weatherInfo = document.getElementById ('result-div')
+// SELECT HTML ELEMENTS USING DOM SELECTORS
 
+var searchInput = document.getElementById("searchInput");
+var submitButton = document.getElementById("submit-btn");
+var weatherInfo = document.getElementById('result-div')
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault(); // cancel the default behavior
+  var cityName = searchInput.value;
+  var API_KEY = '................' //insert the API key here from OpenWeather
 
-submitButton.addEventListener ('click', function(event) {
-    event.preventDefault() // this cancels the default behaviour of refreshing the page as soon as any value is inserted
-    //console.log(searchInput.value)
-    var cityName = searchInput.value; 
-    var API_KEY = '..................' // API key will be inserted here. Not for public
+  if (!cityName) {
+    alert("Please enter your city name");
+  } else {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        response.json().then(function(data){
+            if(data.cod == 404){
+                alert('City not found')
+            }else{
 
-    if (!cityName){
-        alert ('Please enter your city name')
-
-    }
-    else {
-        fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
-
-        ).then(function(response){
-            //console.log(response.json())
-             response.json().then(function(data){
-                    var temperature = data.main.temp 
-                    var location = data.name 
-                    var humidity = data.main.humidity 
-                    var windSpeed = data.wind.speed 
-//dollar sign and backticks for concatenation of variables with strings
-                weatherInfo.innerHTML = ` 
-                <div>
-                    <h1> ${location}</h2> 
-                    <h2> ${temperature}</h2>
-                    <h3> ${location}</h2>
-                    <h4> ${location}</h2>
-                                    </div>`
-                }
-        )
-
-    }
-}
+                var temperature = data.main.temp
+                var location = data.name
+                var humidity = data.main.humidity
+                var windSpeed = data.wind.speed
+                
+                weatherInfo.innerHTML = `
+                <div class='result-div-inner'>
+                    <h1> ${location} </h1> 
+                    <h3> Temperature : ${temperature} °C</h3>
+                    <h3>Humidity : ${humidity} %</h3>
+                    <h3>Wind Speed : ${windSpeed} Km/h </h3>
+                </div>`
+            }
+        })
+        
+        
     })
+  }
+});
